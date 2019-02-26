d0e7cb30c8651be7fe7e8fdb9d178646ee67416c
Daniel Wirtz <danielwirtzx@gmail.com>
2019-02-26 14:57:35 +0100

    :lipstick: Import theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,8 @@
 import React from "react";
 import App, { Container, NextAppContext } from "next/app";
 import { Grommet } from "grommet";
-
-const theme = {
-  global: {
-    colors: {
-      brand: "#228BE6"
-    },
-    font: {
-      family: "Roboto",
-      size: "14px",
-      height: "20px"
-    }
-  }
-};
+// import "inter-ui/inter.css";
+import planulo from "../theme";
 
 class MyApp extends App {
   static async getInitialProps({ Component, ctx }: NextAppContext) {
@@ -28,14 +17,10 @@ class MyApp extends App {
 
   render() {
     const { Component, pageProps } = this.props;
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
-    />;
 
     return (
       <Container>
-        <Grommet theme={theme} full>
+        <Grommet theme={planulo} full>
           <Component {...pageProps} />
         </Grommet>
       </Container>
