c1034201e493466026683f0b9c2e7fb091b7023a
Daniel Wirtz <danielwirtzx@gmail.com>
2019-02-26 12:18:42 +0100

    :construction: Test grommet components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 import App, { Container, NextAppContext } from "next/app";
 import { Grommet } from "grommet";
-import { grommet } from "grommet/themes";
+
+const theme = {
+  global: {
+    colors: {
+      brand: "#228BE6"
+    },
+    font: {
+      family: "Roboto",
+      size: "14px",
+      height: "20px"
+    }
+  }
+};
 
 class MyApp extends App {
   static async getInitialProps({ Component, ctx }: NextAppContext) {
@@ -16,10 +28,14 @@ class MyApp extends App {
 
   render() {
     const { Component, pageProps } = this.props;
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
+    />;
 
     return (
       <Container>
-        <Grommet theme={grommet}>
+        <Grommet theme={theme} full>
           <Component {...pageProps} />
         </Grommet>
       </Container>
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,11 +1,49 @@
 import Header from "../components/header";
 import { NextFC } from "next";
+import { Box, Button, Heading, Grommet } from "grommet";
+import { Notification } from "grommet-icons";
+
+const AppBar = props => (
+  <Box
+    tag="header"
+    direction="row"
+    align="center"
+    justify="between"
+    background="brand"
+    pad={{ left: "medium", right: "small", vertical: "small" }}
+    elevation="medium"
+    style={{ zIndex: "1" }}
+    {...props}
+  />
+);
 
 const About: NextFC = () => (
   <div>
     <Header />
     <h1>About</h1>
     <p>About, yeah!</p>
+    <Box fill>
+      <AppBar>
+        <Heading level="3" margin="none">
+          My App
+        </Heading>
+        <Button icon={<Notification />} onClick={() => {}} />
+      </AppBar>
+      <Box direction="row" flex overflow={{ horizontal: "hidden" }}>
+        <Box flex align="center" justify="center">
+          app body
+        </Box>
+        <Box
+          width="medium"
+          background="light-2"
+          elevation="small"
+          align="center"
+          justify="center"
+        >
+          sidebar
+        </Box>
+      </Box>
+    </Box>
   </div>
 );
 
