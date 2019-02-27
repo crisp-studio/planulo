cac8c3db75c153d052ae77b3d965faaf429ba333
Daniel Wirtz <danielwirtzx@gmail.com>
2019-02-27 14:50:12 +0100

    :wrench: Add Logo and Button

--- components/common/index.tsx
+++ components/common/index.tsx
@@ -1,5 +1,7 @@
 import Container from "./container";
 import Header from "./header";
 import Layout from "./layout";
+import Logo from "./logo";
+import Button from "./button";
 
-export { Layout, Header, Container };
+export { Layout, Header, Container, Logo, Button };
