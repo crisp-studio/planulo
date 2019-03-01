c778b4fa5318ef92218e67a2484a2cbd59c3efdd
Daniel Wirtz <danielwirtzx@gmail.com>
2019-03-01 11:43:51 +0100

    :bento: Add colors and color picker icon

---
+++ components/schedule/event-colors.ts
@@ -0,0 +1,20 @@
+const eventColors = {
+  blue: {
+    text: "#334E68",
+    bg: "#CFE5F5"
+  },
+  yellow: {
+    text: "#334E68",
+    bg: "#FFF3BF"
+  },
+  green: {
+    text: "#334E68",
+    bg: "#D2F4CE"
+  },
+  red: {
+    text: "#334E68",
+    bg: "#F6D6D8"
+  }
+};
+
+export default eventColors;
