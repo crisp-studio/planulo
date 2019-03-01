208d29268595fee5e018b2bc60e2c1fab699b46d
Daniel Wirtz <danielwirtzx@gmail.com>
2019-03-01 11:41:53 +0100

    :fire: Remove Layout Component

--- components/common/index.tsx
+++ components/common/index.tsx
@@ -1,7 +1,6 @@
 import Container from "./container";
 import Header from "./header";
-import Layout from "./layout";
 import Logo from "./logo";
 import Button from "./button";
 
-export { Layout, Header, Container, Logo, Button };
+export { Header, Container, Logo, Button };
--- pages/schedule-editor.tsx
+++ pages/schedule-editor.tsx
@@ -2,20 +2,18 @@ import Schedule from "../components/schedule";
 import ScheduleEditorHeader from "../components/schedule-editor-header";
 import ScheduleHeader from "../components/schedule-header";
 import { Box } from "grommet";
-import { Container, Layout } from "../components/common";
+import { Container } from "../components/common";
 
 const ScheduleEditor = () => (
-  <Layout>
-    <Box direction="column" background="light-1">
-      <Container background="brand">
-        <ScheduleEditorHeader />
-        <ScheduleHeader />
-      </Container>
-      <Container background="light-1">
-        <Schedule />
-      </Container>
-    </Box>
-  </Layout>
+  <Box direction="column" background="light-1">
+    <Container background="brand">
+      <ScheduleEditorHeader />
+      <ScheduleHeader />
+    </Container>
+    <Container background="light-1">
+      <Schedule />
+    </Container>
+  </Box>
 );
 
 export default ScheduleEditor;
